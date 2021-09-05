6d5ed0b95d3f744034eb6648929b3514a6244317
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-09-05 02:37:40 -0300

    Finish application

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -11,44 +11,63 @@ namespace TextEditor
         }
 
         static void Menu()
-        {    
+        {
             Console.Clear();
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("1 - Open the file\n2 - Create a new file\n0 - Exit");
             short option = short.Parse(Console.ReadLine());
 
-            switch(option){
-                case 1: Open() ; break;
-                case 2: Edit() ; break;
+            switch (option)
+            {
+                case 1: Open(); break;
+                case 2: Edit(); break;
                 case 0: System.Environment.Exit(0); break;
-                default: Menu() ; break;
+                default: Menu(); break;
             }
         }
 
-        static void Open(){
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("What is the file's path?");
+            string path = Console.ReadLine();
 
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+
+            Menu();
         }
 
-        static void Edit(){
+        static void Edit()
+        {
             Console.Clear();
             Console.WriteLine("Type your text below (ESC to exit):\n-----------------------------");
             string text = "";
 
-            do{
+            do
+            {
                 text += Console.ReadLine();
                 text += Environment.NewLine;
             }
-            while(Console.ReadKey().Key != ConsoleKey.Escape);
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
 
             Console.Write(text);
         }
 
-        static void Save(string text){
+        static void Save(string text)
+        {
             Console.Clear();
             Console.WriteLine("What is the path's file?");
             var path = Console.ReadLine();
 
-            using(var file = new StreamWriter(path)){
+            using (var file = new StreamWriter(path))
+            {
                 file.Write(text);
             }
 

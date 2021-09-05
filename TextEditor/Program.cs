362b68cbd1a99fae89852b1fcb9f3e9d4f863ff1
unknown <felipefontenele1317@gmail.com>
2021-09-05 00:25:08 -0300

    Implement half of application

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -6,7 +7,53 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {    
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open the file\n2 - Create a new file\n0 - Exit");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option){
+                case 1: Open() ; break;
+                case 2: Edit() ; break;
+                case 0: System.Environment.Exit(0); break;
+                default: Menu() ; break;
+            }
+        }
+
+        static void Open(){
+
+        }
+
+        static void Edit(){
+            Console.Clear();
+            Console.WriteLine("Type your text below (ESC to exit):\n-----------------------------");
+            string text = "";
+
+            do{
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
+        }
+
+        static void Save(string text){
+            Console.Clear();
+            Console.WriteLine("What is the path's file?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path)){
+                file.Write(text);
+            }
+
+            Console.WriteLine($"The file {path} was saved successfully");
+            Menu();
         }
     }
 }

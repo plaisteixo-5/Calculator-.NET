013a492001bde83e4bd3ec8d485838b1e83b1e21
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-09-15 03:30:16 -0300

    Iniciando o projeto de blog

---
+++ Blog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=BLog;User_ID=sa;Password=1q2w3e4r@#$";
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}

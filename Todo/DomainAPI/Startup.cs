649cdf1f72e3d0646dcca8619a1cb54508894d1c
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-14 03:02:03 -0300

    Add dependencies injection

--- Todo/DomainAPI/Startup.cs
+++ Todo/DomainAPI/Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Handlers;
+using Domain.Repositories;
+using DomainInfra.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +20,13 @@ namespace DomainAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Databse"));
+            // services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("")));
+
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

4590ee9998ea7973f3482d94f72c78aa06ef2c76
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-15 03:49:11 -0300

    Fix the validation on TodoHandler

--- Todo/DomainAPI/Controllers/TodoController.cs
+++ Todo/DomainAPI/Controllers/TodoController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using Domain.Commands;
+using Domain.Entities;
 using Domain.Handlers;
+using Domain.Repositories;
 using DomainShared.Commands;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +14,22 @@ namespace DomainAPI.Controllers
     public class TodoController : ControllerBase
     {
         [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAll("felipefon");
+        }
+
+        [Route("")]
         [HttpPost]
         public GenericCommandResult Create(
             [FromBody] CreateTodoCommand command,
             [FromServices] TodoHandler handler
         )
         {
-            command.User = "Felipe Fon";
+            command.User = "felipefon";
             return (GenericCommandResult)handler.Handle(command);
         }
     }

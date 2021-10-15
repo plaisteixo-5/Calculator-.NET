e312768cdbad46a2cae24b7e60ada1bb97ccee1a
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-15 04:15:05 -0300

    Connecting to databse

--- Todo/DomainAPI/Startup.cs
+++ Todo/DomainAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,14 +18,20 @@ namespace DomainAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Databse"));
-            // services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("")));
+            // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Databse"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(
+                Configuration.GetConnectionString("connectionSrting")));
 
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
